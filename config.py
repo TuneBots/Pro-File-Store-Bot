768c1322067a9ff9599fbd4f676927fe6d29f5b7
teamreelsmart <team.reelsmart@gmail.com>
2026-03-04 17:06:34 +0530

    Add timed verify flow with abuse protection and web redirect

--- config.py
+++ config.py
@@ -12,6 +12,10 @@ MSG_EFFECT = 5046509860389126442
 SHORT_URL = "vplink.in" # shortner url 
 SHORT_API = "b4c55b5464676e8a7bbf9e8903b00a289debbec3" 
 SHORT_TUT = "https://t.me/HowToDownloadSnap/2"
+VERIFY_COOLDOWN = int(os.environ.get("VERIFY_COOLDOWN", "30"))
+VERIFY_REDIRECT_DELAY = int(os.environ.get("VERIFY_REDIRECT_DELAY", "5"))
+VERIFY_LOG_CHANNEL = int(os.environ.get("VERIFY_LOG_CHANNEL", "0"))
+SERVICE_URL = os.environ.get("SERVICE_URL", "")
 
 # Bot Configuration
 SESSION = "SnapXPagluBot"
